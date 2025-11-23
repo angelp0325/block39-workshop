fc9c21a962b390ee942bab4f8b937c5657b4304d
Angel Polanco <angelepolanco325@gmail.com>
2025-11-22 19:29:39 -0500

    seed

--- db/seed.js
+++ db/seed.js
@@ -6,5 +6,37 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seed() {
-  // TODO
+  await db.query("DELETE FROM files");
+  await db.query("DELETE FROM folders");
+
+  const folderNames = ["Documents", "Pictures", "Music"];
+  const folderInserts = [];
+
+  for (const name of folderNames) {
+    const { rows } = await db.query(
+      "INSERT INTO folders (name) VALUES ($1) RETURNING *",
+      [name]
+    );
+    folderInserts.push(rows[0]);
+  }
+
+  const fileTemplates = [
+    { name: "file1.txt", size: 1200 },
+    { name: "file2.txt", size: 3400 },
+    { name: "file3.txt", size: 5600 },
+    { name: "file4.txt", size: 7800 },
+    { name: "file5.txt", size: 9100 },
+  ];
+
+  for (const folder of folderInserts) {
+    for (const template of fileTemplates) {
+      const name = `${folder.name.toLowerCase()}_${template.name}`;
+      const size = template.size;
+
+      await db.query(
+        "INSERT INTO files (name, size, folder_id) VALUES ($1, $2, $3)",
+        [name, size, folder.id]
+      );
+    }
+  }
 }
